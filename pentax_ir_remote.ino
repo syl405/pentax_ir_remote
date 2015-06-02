26b4fe7ebc77ace4452816eff42dd87a42efbe57
Shien Yang Lee <syl405@nyu.edu>
2015-06-02 18:05:52 +0800

    Added autofocus capability and renamed variables for consistency. Tested and working

--- pentax_ir_remote.ino
+++ pentax_ir_remote.ino
@@ -20,37 +20,49 @@
 
 #include <multiCameraIrControl.h>
 
-Pentax K5(9);
-const int buttonPin = 2;    // the pin that the pushbutton is attached to
-const int ledPin = 8;        // status led pin
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
+const int shutterButtonPin = 2;    // shutter button pin
+const int focusButtonPin = 12; // focus button pin
+const int statusLEDPin = 8;        // status led pin
+const int infraredLEDPin = 9; // infrared led pin
+int shutterButtonState = 0;         // current state of the shutter button
+int lastShutterButtonState = 0;     // previous state of the shutter button
+int focusButtonState = 0; // current state of the focus button
+int lastFocusButtonState = 0; // previous state of the focus button
+
+Pentax K5(infraredLEDPin); // instantiate Pentax object
 
 void setup(){
-  // initialize the button pin as a input:
-  pinMode(buttonPin, INPUT);
-  // initialize the LED as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize serial communication:
-  Serial.begin(9600);
+  pinMode(shutterButtonPin, INPUT);
+  pinMode(focusButtonPin, INPUT);
+  pinMode(statusLEDPin, OUTPUT); // mode of infrared led pin set during Pentax object instantiation
+  //Serial.begin(9600);
 }
 
 void loop(){
-  // light up ready LED
-  digitalWrite(ledPin, HIGH);
-
-  // read the pushbutton input pin:
-  buttonState = digitalRead(buttonPin);
-
+  // turn off busy LED
+  digitalWrite(statusLEDPin, LOW);
+  
+  // read the focus button input pin:
+  focusButtonState = digitalRead(focusButtonPin);
+  
+  // read the shutter button input pin:
+  shutterButtonState = digitalRead(shutterButtonPin);
+  
   // compare the buttonState to its previous state
-  if (buttonState == HIGH && lastButtonState == LOW){
+  if (focusButtonState == HIGH && lastFocusButtonState == LOW){
+    K5.toggleFocus();
+    digitalWrite(statusLEDPin,HIGH);
+    delay(50); // turn on busy LED for 50 ms
+  }
+  if (shutterButtonState == HIGH && lastShutterButtonState == LOW){
     K5.shutterNow();
-    digitalWrite(ledPin,LOW);
-    delay(500); // extinguish ready LED for 500 ms
+    digitalWrite(statusLEDPin,HIGH);
+    delay(500); // turn on busy LED for 500 ms
   }
 
   // save currentbuttonState for next iteration
-  lastButtonState = buttonState;
+  lastFocusButtonState = focusButtonState;
+  lastShutterButtonState = shutterButtonState;
 }
 
 
